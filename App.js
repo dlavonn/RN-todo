a342008d00001bf157a7bb6171cb9ffaadcda99e
DomLavonn <113148297+dlavonn@users.noreply.github.com>
2022-09-11 20:44:43 -0400

    Finsish todo functionality

--- App.js
+++ App.js
@@ -1,27 +1,97 @@
 import { StatusBar } from "expo-status-bar";
 import { Children, useState } from "react";
-import { Alert, StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Pressable, 
+  FlatList
+} from "react-native";
 import ModalName from "./components/modal/Modal";
+import ListStyle from "./components/liststyle/ListStyle";
 
 export default function App() {
-  
+  const [enterData, setEnterData] = useState(["Walking dog", "Clean kitchen"])
+  const [enterText, setEnterText] = useState("");
   const [childObj, setChildObj] = useState({
-    name: '',
-    modalShow:true
-  })
- 
+    name: "",
+    modalShow: true,
+  });
+
   const collectData = (obj) => {
     // Updating object
-     setChildObj({ ...childObj, name:obj.name, modalShow: false})
-    
-  
-  }
+    setChildObj({ ...childObj, name: obj.name, modalShow: false });
+  };
+
+  const AddItemHandlar = () => {
+
+     
+    setEnterData( ps =>{
+
+       return [...ps, enterText]
+    }
+    ) 
+
+    setEnterText('')
+
+  };
 
   return (
-    <View style={[ styles.container, childObj.modalShow ?  styles.containerb : styles.containerw]} >
-      <ModalName collectData={collectData}/>
+    <View
+      style={[
+        styles.container,
+        childObj.modalShow ? styles.containerb : styles.containerw,
+      ]}
+    >
+      <ModalName collectData={collectData} />
 
-      <Text> {childObj.name} Please Enter your Daily Task!</Text>
+      <Text style={{ position: "absolute", top: 60, fontSize: 16 }}>
+        {" "}
+        {childObj.name} Please Enter your Daily Task!
+      </Text>
+
+      {childObj.name != "" && (
+        <View>
+          <View style={styles.enterInfo}>
+            <TextInput
+              style={styles.textI}
+              onChangeText={setEnterText}
+              value={enterText}
+            />
+            <Button
+              style={styles.btn}
+              title="Enter Item"
+              color="red"
+              onPress={AddItemHandlar}
+            />
+          </View>
+          <View style={styles.tester}>
+          <FlatList
+        data={enterData}
+        renderItem={ ({item, index}) => {
+
+          return (
+          <>
+          <ListStyle  enterData={item} />
+          </>
+
+          )
+        }}
+      
+      />
+            </View>
+
+     
+
+
+
+       
+    
+        </View>     
+      )}
     </View>
   );
 }
@@ -36,7 +106,25 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
   containerb: {
-    backgroundColor: 'black'
+    backgroundColor: "black",
+  },
+  enterInfo: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  textI: {
+    borderColor: "black",
+    borderWidth: 1,
+    margin: 20,
+    width: 250,
+  },
+  btn: {
+    width: "20px !important",
+    height: 55,
+    paddingHorizontal: 19,
+  },
+  tester:{
+    flexDirection: "column"
   }
-
 });
---
+++ components/liststyle/ListStyle.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ListStyle( props) {
+  return (
+    <View>
+      <Text>{props.enterData}</Text>
+    </View>
+  )
+}
